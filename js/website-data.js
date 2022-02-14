7dadb670ddf1daa356f2aa0db727cac72cd60221
Yana <yanaegorova69.3@gmail.com>
2022-02-14 22:29:08 +0200

    +

--- js/website-data.js
+++ js/website-data.js
@@ -73,7 +73,8 @@ function setInformation(array, info) {
 
 function setWebsiteHref(array, info) {
     array.forEach(item => {
-       item.hasAttribute('href') && item.setAttribute('href', info);
+        console.log('item', item);
+       item.hasAttribute('href') && item.setAttribute('href', `http://${info}`);
     });
 }
 
